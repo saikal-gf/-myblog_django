8899412256cf82ff0287039b397a57bd91cd7606
Saikal <abdibalievasajkal5@gmail.com>
2025-05-16 14:00:14 +0600

    Update models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Содержание')
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.title
